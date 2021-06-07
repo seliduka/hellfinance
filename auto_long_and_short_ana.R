1f78ed1b6739e48907f07792d18be6249d2cfe10
seliduka <seliduka@gmail.com>
2021-06-07 16:45:35 +0800

    2021/06/07

--- auto_long_and_short_ana.R
+++ auto_long_and_short_ana.R
@@ -8,9 +8,11 @@ showtext_auto(enable=T)
 thexml <- read.xlsx("more.xlsx",sheetName = "Sheet1" )
 thexml$NA. <- NULL
 thexml$date <- as.Date(thexml$date, format =  "%Y-%m-%d")
+thexml <- thexml[-c(14),]
+dday <- nrow(thexml)
 ggplot(thexml, aes(x=date)) + geom_line(aes(y = numb), color = "darkred")
 
-dday <- nrow(thexml)
+
 
 HU <- getSymbols("^TWII", auto.assign = FALSE, from = "2017-01-01")
 HU <- HU[(rowSums(is.na(HU)) == 0), ]
@@ -24,7 +26,11 @@ rownames(HU) <- 1:nrow(HU)
 HU <- cbind(symbol = "GG", HU)
 HU$date <- as.Date(HU$date, format =  "%Y-%m-%d")
 HU <- as.tibble(HU)
-thexml <- cbind(thexml, HU$close)
-thexml$`HU$close`<- thexml$`HU$close`-16000
-ggplot(thexml, aes(x=date)) + geom_line(aes(y = numb), color = "darkred") + geom_line(aes(y = thexml$`HU$close`), color = "blue")
+thexml$move <- ((HU$close-HU$low))*100
+thexml$close <- (HU$close-17000)*10
+
+ggplot(thexml, aes(x=date)) +
+  geom_line(aes(y = numb), color = "darkred") +
+  geom_line(aes(y = close), color = "green") +
+  geom_line(aes(y = move), color = "blue")
 
